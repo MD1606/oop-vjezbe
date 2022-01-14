0dd4b9a469c629c8fb2d3e3cd83a6ce305df4f1e
MD1606 <martina.drazetic1@gmail.com>
2022-01-14 19:47:52 +0100

    aaa

--- VJEZBA5_OOP_neww/vjezba5/vjezba5/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Header.hpp"
-int main()
-{
-	srand(time(NULL));
-	position c1;
-	double x, y, z;
-	c1.get(x, y, z); //dohvacamo clanove s funkcijom get iz klase position
-	c1.game();
-}
--- VJEZBA6_OOP_new1/vjezba6_HangMan/vjezba6_HangMan/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Header.hpp"
-
-int main() {
-	HangmanModel m;
-	m.set_game();
-	HangmanView v(m);
-	HangmanController c(m, v);
-	c.game();
-}
